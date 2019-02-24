c7cfca4965b94a0aa9a81640e268ed08d08359ac
Olga Soloveva <solo_o_e@mail.ru>
2019-02-24 21:10:52 +0300

    init database, make @db

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -9,4 +19,9 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  content = params[:content]
+  erb "You typed: #{content}"
 end
