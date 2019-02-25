2355d966965115e1a9f8fc3831ed2d9807e8c67d
Olga Soloveva <solo_o_e@mail.ru>
2019-02-25 19:55:50 +0300

    Add textarea to /details/...

--- app.rb
+++ app.rb
@@ -70,8 +70,11 @@ end
 # вывод информации о посте
 
 get '/details/:post_id' do
+	# получаем переменную из url'a
 	post_id = params[:post_id]
 
+	# получаем список постов
+	# (у нас будет только один пост)
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
 	@row = results [0]
 
