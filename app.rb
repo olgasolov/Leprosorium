46c21bdfbca10fa9ee3a9fbd3a4edf6c4c1754de
Olga Soloveva <solo_o_e@mail.ru>
2019-02-25 20:08:12 +0300

    Add simple /details/... post handler

--- app.rb
+++ app.rb
@@ -76,7 +76,23 @@ get '/details/:post_id' do
 	# получаем список постов
 	# (у нас будет только один пост)
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
+
+	# выбираем этот один пост в переменную @row
 	@row = results [0]
 
+	# возвращаем представление details.erb
 	erb :details
+end
+
+# обработчик post-запроса /details/...
+# (браузер отправляет данные на сервер, мы их принимаем)
+
+post '/details/:post_id' do
+	# получаем переменную из url'a
+	post_id = params[:post_id]
+
+	# получаем переменную из post-запроса
+	content = params[:content]
+
+	erb "You typed coment #{content} for post #{post_id}"
 end
